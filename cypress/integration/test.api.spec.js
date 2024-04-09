aa0d3f8a4e51a64af0a24f2d20d37e67f5ae2b62
DieroKreator <qtechwired@gmail.com>
2024-04-09 09:25:35 -0300

    Renamed test

--- cypress/integration/test.api.spec.js
+++ cypress/integration/test.api.spec.js
@@ -1,9 +1,12 @@
-describe('template spec', () => {
-    it('passes', () => {
-    //Getting response from browserstack demo website 
-    cy.request('GET','/v2/pet/191').then((response) =>{
-    //Expecting the response status code to be 200
-    expect(response.status).to.eq(200)
-    })
-    })
+describe('API Testing', () => {
+
+    it('API - GET - Find pet by ID', () => {
+            //Getting response from petstore demo website 
+            cy.request('GET',
+                        '/v2/pet/191'
+                        ).then((response) =>{
+                //Expecting the response status code to be 200
+                expect(response.status).to.eq(200)
+            })
+        })
     })
